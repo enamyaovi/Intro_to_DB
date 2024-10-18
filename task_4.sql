6db46da4952a89179d57721b89a90d719173a7d1
Cephas Tay <cephas.tay137@gmail.com>
2024-10-18 18:02:49 +0000

    Corrected Query for Describing Book Table

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,12 @@
-SHOW COLUMNS FROM Books FROM alx_book_store;
+-- SHOW COLUMNS FROM Books FROM alx_book_store;
+USE alx_book_store;
+
+SELECT 
+    TABLE_NAME, 
+    COLUMN_NAME, 
+    COLUMN_TYPE
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
