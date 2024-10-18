0bda1662ded25cb6850b788d885bf58c2e4ea22e
Cephas Tay <cephas.tay137@gmail.com>
2024-10-18 18:18:10 +0000

    Removed alias on mysql.connector

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,4 +1,4 @@
-import mysql.connector as mySQL
+import mysql.connector
 from getpass import getpass
 
 config = {
@@ -9,16 +9,16 @@ config = {
 
 
 try:
-    with mySQL.connect(**config) as Cnx:
+    with mysql.connector.connect(**config) as Cnx:
         with Cnx.cursor() as myCursor:
             try:
                 myCursor.execute( "CREATE DATABASE IF NOT EXISTS alx_book_store;" , multi=True)
                 print("Database 'alx_book_store' created successfully!")
                 Cnx.commit()
-            except mySQL.DatabaseError as e:
+            except Cnx.DatabaseError as e:
                 print(e)
-except mySQL.Error as e:
-  if e.errno == mySQL.errorcode.ER_ACCESS_DENIED_ERROR:
+except mysql.Error as e:
+  if e.errno == mysql.errorcode.ER_ACCESS_DENIED_ERROR:
     print("Something is wrong with your password")
   else:
     print(e)
