634933bdb7eada17a3c52f9db938822e17107918
Cephas Tay <cephas.tay137@gmail.com>
2024-10-18 18:23:36 +0000

    corrected exeception error handling

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,6 +1,8 @@
 import mysql.connector
 from getpass import getpass
 
+import mysql.connector.errorcode
+
 config = {
     'host' : '172.17.176.1',
     'user' : 'enamyaovi',
@@ -15,10 +17,10 @@ try:
                 myCursor.execute( "CREATE DATABASE IF NOT EXISTS alx_book_store;" , multi=True)
                 print("Database 'alx_book_store' created successfully!")
                 Cnx.commit()
-            except Cnx.DatabaseError as e:
+            except mysql.connector.DatabaseError as e:
                 print(e)
-except mysql.Error as e:
-  if e.errno == mysql.errorcode.ER_ACCESS_DENIED_ERROR:
+except mysql.connector.Error as e:
+  if e.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
     print("Something is wrong with your password")
   else:
     print(e)
